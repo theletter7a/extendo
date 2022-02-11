df22a8139c441bb38f78890838e07629e4043d17
Z <ouhassel@gmail.com>
2022-02-11 10:06:25 -0500

    manifestify generates manifest content

---
+++ lib/filemanager.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+import 'package:yaml/yaml.dart';
+void writeManifest(String dir, String content){
+
+}
+
+void writeContent(String content){
+}
---
+++ lib/manifestify.dart
@@ -0,0 +1,22 @@
+import 'package:yaml/yaml.dart';
+import 'dart:io';
+
+String manifestify(String content){
+  Map options = loadYaml(content);
+
+  Map manifest = {
+    'name': options['name'],
+    'manifest_version': 2,
+    'version': '1.0.0',
+    'content_scripts':[
+      {
+        'matches': ['<all_urls>'],
+        'js': ['content.js']
+      }
+    ]
+  };
+  if(options['matches']!=null){
+    manifest[options['matches']];
+  }
+  return manifest.toString();
+}
